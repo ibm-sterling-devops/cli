7161e3123230f6bd92d74e6fb6a5db8b24576cfe
Enio Basso <ebasso@ebasso.net>
2023-11-27 18:58:23 -0300

    version 0.1

--- home/custom-script.py
+++
@@ -1,8 +0,0 @@
-# This script allows you to ....
-import os
-
-if __name__ == "__main__":
-#    if "DEVOPS_MONGO_URI" not in os.environ or os.environ['DEVOPS_MONGO_URI'] == "":
-#        sys.exit(0)
-
-    print("Custom Script")
